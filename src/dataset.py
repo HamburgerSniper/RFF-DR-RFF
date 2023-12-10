91882eaad0d70a6247f45a7b778d4b13ba5c203d
HamburgerSniper <1505189749@qq.com>
2023-12-10 14:38:41 +0800

    doc: FIR滤波器 代码解读

--- src/dataset.py
+++ src/dataset.py
@@ -2,26 +2,56 @@ import os
 
 import torch
 
-from .preprocessing import main as main_NMP
-from .preprocessing_MP import main as main_MP
-from .utils import TorchComplex as tc
+from preprocessing import main as main_NMP
+from preprocessing_MP import main as main_MP
+from utils import TorchComplex as tc
+
+"""
+    FIR 滤波器实现：FIR滤波器是一种线性时不变的滤波器，其冲激响应在时域上是有限的,通过将输入信号与滤波器的系数进行线性卷积运算，对输入信号进行滤波处理。
+    FIR滤波器常用于信号处理领域，可以根据需要对信号进行频率选择性滤波，去除其中的噪声和不需要的频率分量。它可以通过调整滤波器的系数来实现对信号的不同频率分量的强弱程度的控制。
+    在射频指纹信号处理中，使用FIR滤波器主要考虑到其以下几种优点：
+        (1)线性相位：在处理射频指纹信号时，我们需要保持信号的相位信息，以便于后续的分析和识别。FIR滤波器可以通过设计其系数，使其具有严格的线性相位特性，从而在滤波过程中不会引入额外的相位失真。
+        (2)稳定特性：由于FIR滤波器的冲激响应是有限长的，因此它是一个稳定的系统，可以保证滤波结果的稳定输出。这对于射频指纹信号的处理尤为重要，因为我们需要稳定的信号来进行准确的分析和识别。
+        (3)设计灵活性：FIR滤波器可以通过调整其系数来改变其频率响应特性，从而实现不同的滤波效果。这对于射频指纹信号的处理来说，可以根据实际需求灵活地设计滤波器，以达到最佳的滤波效果。
+"""
+
+"""
+    x: 输入信号，其形状为 (batch_size, N, 2)，其中 N 是信号的长度，2 是信号的通道数（例如，对于QPSK调制，一个通道表示实部，另一个通道表示虚部）
+    taps:  表示滤波器的阶数，默认值为 9。这意味着滤波器的冲击响应长度为 9
+    padding: 添加到输入信号两端的零的数量，以确保滤波器可以覆盖整个信号
+    x_pad: 添加了零填充的输入信号
+    h: 滤波器的冲击响应，这里使用随机高斯分布生成，然后除以 (taps * 2) ** 0.5 以保证滤波器的功率为 1。
+    x_list: 是一个列表，用于存储滤波器冲击响应与输入信号的卷积结果。
+    X: 卷积结果的堆叠，形状为 (N, taps, 2)。
+    x_FIR: 滤波器的输出，通过将 X 与 h 相乘得到。
+    
+    最后，x_FIR 被重新塑形为 (1, N, 2)，以便于返回
+"""
 
 
+# 定义FIR 接收参数为 输入信号x 和 滤波器阶数，默认为 9
 def FIR(x, taps=9):
+    # 获取输入信号x的形状，这里的形状为(batch_size, N, 2)，其中N是信号的长度，2是信号的通道数（例如，对于QPSK调制，一个通道表示实部，另一个通道表示虚部）
     _, N, _ = x.shape
+    # 计算需要添加到输入信号两端的零的数量，以确保滤波器可以覆盖整个信号
     padding = int((taps - 1) / 2)
+    # 添加零填充到输入信号的两端，以创建一个更大的信号，使得滤波器的冲击响应可以覆盖整个信号
     x_pad = torch.cat(
         [
             torch.zeros(padding, 2, dtype=x.dtype),
             x.view(N, 2),
             torch.zeros(padding, 2, dtype=x.dtype)
         ], dim=0)
+    # 生成滤波器的冲击响应，这里使用随机高斯分布生成，然后除以 (taps * 2) ** 0.5 以保证滤波器的功率为 1
     h = torch.randn(taps, 1, 2, dtype=x.dtype) / (taps * 2) ** 0.5
+    # 创建一个列表，用于存储滤波器冲击响应与输入信号的卷积结果
     x_list = []
     for i in range(N):
         x_list.append(x_pad[i:i + taps, :])
     X = torch.stack(x_list)
+    # 将列表中的元素堆叠成一个张量，形状为(N, taps, 2)
     x_FIR = tc.mm(X, h)
+    # 将滤波器的输出重塑为(1, N, 2)的形状，并返回
     return x_FIR.view(1, N, 2)
 
 
